748cc16c7ac9f236f3e301de08e61c34cd729096
Valerio Mariani <valmar@slac.stanford.edu>
2025-07-10 17:19:50 -0700

    First commit on 07/10/25

---
+++ README.md
@@ -0,0 +1,16 @@
+# LCLS II development repository [![Build Status](https://travis-ci.org/slac-lcls/lcls2.svg?branch=master)](https://travis-ci.org/slac-lcls/lcls2)
+
+## Build instructions:
+
+```bash
+
+# build all packages in the repository and install them in ./install, option to choose build type
+# most developers can eliminate all the arguments to build_all.sh
+source setup_env.sh
+./build_all.sh -c {Release, Debug, RelWithDebInfo} -p {develop, install}
+
+```
+
+To run the psana automated tests run "pytest psana/psana/tests/" in your git root directory.
+
+You can read the above build_all.sh script to see how to build individual packages.
---
+++ clean_lcls2.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Clean lcls2 repository from files genetated during build" 
+
+REPODIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo REPODIR: $REPODIR
+
+echo rm -rf $REPODIR/install
+     rm -rf $REPODIR/install
+
+echo rm -rf $REPODIR/psana/{build,*.egg-info,*.so,psana/*.so}
+     rm -rf $REPODIR/psana/{build,*.egg-info,*.so,psana/*.so}
+
+echo find . -name "*~" -delete
+     find . -name "*~" -delete
+
+echo find . -name "*.pyc" -delete
+     find . -name "*.pyc" -delete
+
+echo find . -name build -type d -exec rm -rf {} +
+     find . -name build -type d -exec rm -rf {} +
+
+echo find . -name __pycache__ -type d -exec rm -rf {} +
+     find . -name __pycache__ -type d -exec rm -rf {} +
+
+echo find . -name dist -type d -exec rm -rf {} +
+     find . -name dist -type d -exec rm -rf {} +
+
+echo "Cleaning is done"
